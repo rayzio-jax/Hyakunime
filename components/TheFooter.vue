472b52d849246b639df9d5b86c60bdc349f71eb3
rayhan <rayhananthaakbar@gmail.com>
2023-10-13 16:53:26 +0700

    update: added web icon

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,11 @@
 <template>
   <footer id="footer" class="@footer p-10 bg-primary-focus text-white">
     <aside>
+      <img
+        src="~/assets/image/hyakunime.png"
+        alt="hyakunime-banner"
+        class="object-cover min-w-0 w-[400px] h-full md:h-[70px] md:max-w-max md:min-w-max"
+      />
       <p>hyakuNime ( 百ニメ )</p>
       <p>All rights reserved &copy; Copyright 2023.</p>
     </aside>
