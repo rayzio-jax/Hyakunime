496384a6f769e0c3f1d6365d3a8c77a05a9c7b61
rayhan <rayhananthaakbar@gmail.com>
2023-10-16 00:29:32 +0700

    added: trakteer button

---
+++ components/TrakteerButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    class="text-lg font-onest relative border-2 border-red-500 text-red-500 focus:focus-within:outline-none focus:focus-within:bg-red-500 focus:focus-within:text-white hover:bg-red-500 hover:border-red-500 hover:text-white @btn @btn-outline @btn-md dark:bg-red-500 dark:text-white dark:hover:bg-red-700 dark:hover:border-red-700 dark:focus:focus-within:border-white dark:focus:focus-within:bg-red-700"
+    href="https://trakteer.id/rayziojax/tip"
+    target="_blank"
+  >
+    <ion-icon name="wallet-outline" class="animate-bounce"></ion-icon>
+    Donasi bang
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'TrakteerButton',
+}
+</script>
